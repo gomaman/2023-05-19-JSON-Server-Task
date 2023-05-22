ae9c56dd3d7e227f9391d725b6891ae49f1654e7
gomaman <999tmll@gmail.com>
2023-05-22 13:36:43 +0300

    gMap oops fix

--- user-script.js
+++ user-script.js
@@ -1,11 +1,3 @@
-//!  5. Sukurti naują puslapį user.html, kuriame bus atvaizduojama vartotojo informacija:
-//!  5.1. Pilnas vardas.
-//!  5.2. Vartotojo vardas / nick'as.
-//!  5.3. El. paštas.
-//!  5.4. Adresas, kuris turės gatvę, namo numerį, miestą, pašto kodą. Paspaudus ant adreso, pagal koordinates, turėtų atidaryti šios vietos Google Maps.
-//!  5.5. Telefono numeris.
-//!  5.6. Internetinio puslapio adresas.
-//!  5.7. Įmonės, kurioje dirba, pavadinimas.
 
 async function init() {
   let response = await fetch("https://jsonplaceholder.typicode.com/users/1");
@@ -35,25 +27,20 @@ function singleUserGenerator(userData) {
   let nameItem = document.createElement("li");
   let usernameItem = document.createElement("li");
   let emailItem = document.createElement("li");
-    let addressItems = document.createElement("ul");
-        let streetItem = document.createElement("li");
-        let suiteItem = document.createElement("li");
-        let cityItem = document.createElement("li");
-        let zipCodeItem = document.createElement("li");
+  let addressItems = document.createElement("li");
+  let addressLink = document.createElement("a");
   let phoneItem = document.createElement("li");
   let websiteItem = document.createElement("li");
   let companyNameItem = document.createElement("li");
 
-  streetItem.textContent = street;
-  suiteItem.textContent = suite;
-  cityItem.textContent = city;
-  zipCodeItem.textContent = zipCode;
+  addressLink.innerHTML = `<b>Address:</b> ${street} ${suite} ${city} ${zipCode}`;
 
-  addressItems.append(
-    streetItem,
-    suiteItem,
-    cityItem,
-    zipCodeItem);
+  addressLink.setAttribute(
+    "href",
+    `https://www.google.com/maps?q=${userData.address.geo.lat},${userData.address.geo.lng}`
+  );
+
+  addressItems.append(addressLink);
 
   body.append(userUnorderedList);
   userUnorderedList.append(
@@ -77,41 +64,39 @@ function singleUserGenerator(userData) {
 init();
 
 function nav() {
-    let body = document.querySelector("body");
-    let header = document.createElement('header')
-    let navigation = document.createElement("nav");
-  
-    body.prepend(header);
-    header.prepend(navigation)
-  
-    let home     = document.createElement("a");
-    let users    = document.createElement("a");
-    let albums   = document.createElement("a");
-    let posts    = document.createElement("a");
-    let comments = document.createElement("a");
-    let user     = document.createElement("a");
-  
-    navigation.prepend(home,users,albums,posts,comments,user)
-  
-    home.setAttribute('href','index.html')
-    home.textContent = 'Home'
-  
-    users.setAttribute('href','users.html')
-    users.textContent = 'Users'
-  
-    albums.setAttribute('href','albums.html')
-    albums.textContent = 'Albums'
-  
-    posts.setAttribute('href','posts.html')
-    posts.textContent = 'Posts'
-  
-    comments.setAttribute('href','comments.html')
-    comments.textContent = 'Comments'
+  let body = document.querySelector("body");
+  let header = document.createElement("header");
+  let navigation = document.createElement("nav");
 
-    user.setAttribute('href','user.html')
-    user.textContent = 'User/Test'
+  body.prepend(header);
+  header.prepend(navigation);
 
+  let home = document.createElement("a");
+  let users = document.createElement("a");
+  let albums = document.createElement("a");
+  let posts = document.createElement("a");
+  let comments = document.createElement("a");
+  let user = document.createElement("a");
 
-  }
-  
-  nav();
+  navigation.prepend(home, users, albums, posts, comments, user);
+
+  home.setAttribute("href", "index.html");
+  home.textContent = "Home";
+
+  users.setAttribute("href", "users.html");
+  users.textContent = "Users";
+
+  albums.setAttribute("href", "albums.html");
+  albums.textContent = "Albums";
+
+  posts.setAttribute("href", "posts.html");
+  posts.textContent = "Posts";
+
+  comments.setAttribute("href", "comments.html");
+  comments.textContent = "Comments";
+
+  user.setAttribute("href", "user.html");
+  user.textContent = "User/Test";
+}
+
+nav();
